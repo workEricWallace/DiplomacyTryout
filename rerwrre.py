0bc0b4dc80993ff86c5dc05f002bb4ae5e411fd7
Eric Wallace <eswallace@comcast.net>
2017-07-25 23:17:56 -0400

    Update rerwrre.py

--- rerwrre.py
+++ rerwrre.py
@@ -6,8 +6,16 @@ import os
 import urllib
 
 import numpy as np
-import tensorflow as tf
+#imcport tensorflow as tf
 import sklearn.model_selection as sk
+
+#throw out data that is during the betrayal and 1 friendship before it (if it was a betrayal)
+def trimDataSet(diplomacy):
+    for entry in diplomacy:
+        if (entry ['betrayal'] == True):
+            del entry['seasons'][len(entry['seasons']) - 1]
+            del entry['seasons'][len(entry['seasons']) -1]
+    return diplomacy
 
 
 if __name__ == "__main__":
@@ -19,31 +27,77 @@ if __name__ == "__main__":
 
     outputData = []
     features = []
+    diplomacy = trimDataSet(diplomacy)
+
+    sentimentPositive = 0
+    numSentences = 0
+    negativeSentiment = 0
+    numWords = 0
+    DMFuture = 0
+    DMContingency = 0
+    DMExpansion = 0
+    DMComparison = 0
+
     for entry in diplomacy:
-        if (entry ['betrayal'] == betrayalFlag):
-            outputData = outputData.append(outputData, 1)
+        if (entry ['betrayal'] == True):
+            for x in range (len(entry['seasons'])):
+                outputData.append(1)
         else:
-            outputData = outputData.append(outputData, 0)
+            for x in range (len(entry['seasons'])):
+                outputData.append(0)
         for season in entry['seasons']:
             for betrayerMessage in season['messages']['betrayer']:
-                features = features.append(features, (betrayerMessage['sentiment']['positive'] / betrayerMessage['n_sentences']))
-                features = features.append(features, betrayerMessage['n_sentences'])
-                features = features.append(features, betrayerMessage['n_words'])
-                features = features.append(features, (betrayerMessage['sentiment']['negative'] / betrayerMessage['n_sentences']))
+                numSentences = numSentences + betrayerMessage['n_sentences']
+                numWords = numWords + betrayerMessage['n_words']
+                negativeSentiment = negativeSentiment + betrayerMessage['sentiment']['negative']
+                sentimentPositive = sentimentPositive + betrayerMessage['sentiment']['positive']
 
-                dict = Messages['lexicon_words']
+                dict = betrayerMessage['lexicon_words']
                 if 'disc_temporal_future' in dict:
-                    features = features.append(features, (len(dict['disc_temporal_future']) / betrayerMessage['n_sentences']))
-                if 'disc_expansion' in dict:
-                    features = features.append(features, (len(dict['disc_expansion']) / betrayerMessage['n_sentences']))
+                    DMFuture = DMFuture + (len(dict['disc_temporal_future']))
+                if 'disc_expansion' in dict:                
+                    DMExpansion = DMExpansion + (len(dict['disc_expansion']))
                 if 'disc_comparison' in dict:
-                    features = features.append(features, (len(dict['disc_comparison']) / betrayerMessage['n_sentences']))
+                    DMComparison = DMComparison + (len(dict['disc_comparison']))
                 if 'disc_temporal_contingency' in dict:
-                    features = features.append(features, (len(dict['disc_temporal_contingency']) / betrayerMessage['n_sentences']))
-                
+                    DMContingency = DMContingency + (len(dict['disc_temporal_contingency']))
+
+            features.append(numSentences)
+            features.append(numWords)
+            
+            if (numSentences != 0): 
+                features.append(sentimentPositive / numSentences)
+                features.append(negativeSentiment / numSentences)
+                features.append(DMFuture / numSentences)
+                features.append(DMExpansion / numSentences)
+                features.append(DMComparison / numSentences)
+                features.append(DMContingency / numSentences)
+
+            else:
+                features.append(0)
+                features.append(0)
+                features.append(0)
+                features.append(0)
+                features.append(0)
+                features.append(0)
+            sentimentPositive = 0
+            numSentences = 0
+            negativeSentiment = 0
+            numWords = 0
+            DMFuture = 0
+            DMContingency = 0
+            DMExpansion = 0
+            DMComparison = 0
+
+            
+
     
   #break data into train and testing sets, with 80% for test set
-    features = np.arrange(4000).reshape((8,500))
+    inputData =  np.asarray(features)
+    print(inputData)
+    print(len(features))
+    print(len(outputData))
+    inputData = np.reshape(inputData, (8,2794))
     
  # outputData = 
  # X_train, X_test, y_train, y_test = train_test_split(features, outputData, test_size=0.20, random_state=18)
